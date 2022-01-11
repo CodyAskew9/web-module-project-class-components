acfa3684ea482a0b2d9e9551df43e67d0234e3e0
cody askew <codyaskew9@gmail.com>
2022-01-10 20:11:15 -0700

    jobs done

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Todo extends React.Component {
+    render() {
+        return(
+            <li>
+                {this.props.todo.task}
+            </li>
+            )
+    }
+}
+export default Todo;
